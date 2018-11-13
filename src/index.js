0281aaa13cc44bf9dc0e20762771277340e798f9
EvanAlto <altoevanb@gmail.com>
2018-11-13 14:59:13 -0600

    brought in mapbox-gl and created webpack.config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('index.js works')
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWx0b2V2YW5iIiwiYSI6ImNqb2c3cWd5cTBjOGMzdmxqenl1Y3N3cnYifQ.QcakT_w4-gXfnShSoPG_1A';
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    center: [-87.6354,41.8885],
+    zoom: 12,
+    style: 'mapbox://styles/mapbox/streets-v10'
+});
